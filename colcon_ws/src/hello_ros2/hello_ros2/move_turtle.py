e758b2111ca32c681d008d04c7a42c83b7618dfc
freshmea <freshmea@naver.com>
2025-05-10 14:25:49 +0900

    추가: move_turtle.py 파일에서 각도 및 선형 속도 수정, turtlesim.yaml 및 moveTurtle.launch.py 파일 추가

--- colcon_ws/src/hello_ros2/hello_ros2/move_turtle.py
+++ colcon_ws/src/hello_ros2/hello_ros2/move_turtle.py
@@ -1,4 +1,4 @@
-#/turtle1/cmd_vel [geometry_msgs/msg/Twist]
+# /turtle1/cmd_vel [geometry_msgs/msg/Twist]
 import rclpy
 from geometry_msgs.msg import Twist
 from rclpy.node import Node
@@ -6,15 +6,15 @@ from rclpy.node import Node
 
 class Move_turtle(Node):
     def __init__(self):
-        super().__init__('move_turtle') # node name
+        super().__init__("move_turtle")  # node name
         self.create_timer(0.1, self.pub_turtle)
         self.pub = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
         self.vel = 0.0
 
     def pub_turtle(self):
         msg = Twist()
-        msg.angular.z = 0.5 # python type 캐스팅이 자유롭다.
-        msg.linear.x = self.vel # 하지만 DDS 로 넘길 때는 type check 가 되어야 한다.
+        msg.angular.z = 1.2  # python type 캐스팅이 자유롭다.
+        msg.linear.x = self.vel  # 하지만 DDS 로 넘길 때는 type check 가 되어야 한다.
         self.pub.publish(msg)
         self.vel += 0.01
 
@@ -27,5 +27,6 @@ def main():
     except KeyboardInterrupt:
         node.destroy_node()
 
-if __name__== '__main__':
+
+if __name__ == "__main__":
     main()
