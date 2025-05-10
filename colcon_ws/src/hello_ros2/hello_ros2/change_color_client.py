855f99441971a55454a0ff106270ee89ddd8f3cb
freshmea <freshmea@naver.com>
2025-05-10 14:40:09 +0900

    추가: change_color_client.py 파일 생성 및 서비스 클라이언트 관련 주석 추가

---
+++ colcon_ws/src/hello_ros2/hello_ros2/change_color_client.py
@@ -0,0 +1,6 @@
+# service client 을 만드세요.
+# service 이름 타입(turtlesim_node color 변경) 실시간으로 다양한 색상
+
+# launch  파일에 노드 추가 moveTurtle.launch.py
+# move_turtle.py -> 파라미터를 설정 각속도 움직임을 외부에서 변화!
+# 위 파라미터도 yaml 에 넣어서 작동!
