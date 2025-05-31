729937b49761e0acbea117c3ad953c4262acf8f7
freshmea <freshmea@naver.com>
2025-05-31 16:41:03 +0900

    수정: publish_map.py에서 맵 퍼블리시 기능 개선 및 데이터 초기화 수정

--- colcon_ws/src/hello_ros2/hello_ros2/publish_map.py
+++ colcon_ws/src/hello_ros2/hello_ros2/publish_map.py
@@ -1,3 +1,7 @@
+# ros2 topic echo /map --once
+# ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 map odom - tf 임이 발행
+# ros2 run hello_ros2 publish_map
+
 import rclpy
 from nav_msgs.msg import OccupancyGrid
 from rclpy.node import Node
@@ -6,10 +10,10 @@ from rclpy.node import Node
 class PublishMap(Node):
     def __init__(self):
         super().__init__("publish_map")  # node name
-        self.create_timer(0.01, self.pub_cb)
-        self.pub = self.create_publisher(OccupancyGrid, "/map", 10)
+        self.create_timer(0.5, self.pub_cb)
+        self.pub = self.create_publisher(OccupancyGrid, "/map", 100)
         self.msg = OccupancyGrid()
-        self.msg.header.frame_id = "test map"
+        self.msg.header.frame_id = "odom"
         self.msg.info.resolution = 0.1
         self.msg.info.width = 200
         self.msg.info.height = 100
@@ -21,7 +25,8 @@ class PublishMap(Node):
         self.msg.info.origin.orientation.z = 0.0
         self.msg.info.origin.orientation.w = 1.0
 
-        self.msg.data = [-1 for _ in range(20_000)]
+        self.msg.data = [100 for _ in range(10_000)]
+        self.msg.data.extend([0 for _ in range(10_000)])
 
     def pub_cb(self):
         self.msg.header.stamp = self.get_clock().now().to_msg()
